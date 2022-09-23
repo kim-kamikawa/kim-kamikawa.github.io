0d445a7202c48c593324d480a2fba6caa20b1ef4
kim-kamikawa <kim.kamikawa@outlook.com>
2022-09-22 19:39:18 -0500

    add comments

--- js/entry.js
+++ js/entry.js
@@ -1,7 +1,10 @@
 function validateEmail(e) {
-	// (at least one number or letter)@(at least one number or letter).(2 letters)
+	// Event handler for #entryForm submit event
+	// A valid email is (at least one number or letter)@(at least one number or letter).(2 letters)
 	let regex = /[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[a-zA-z]{2}/;
+	// If email validation fails...
 	if (!regex.test(document.getElementById("email").value)) {
+		// ... change placeholder message and prevent form submission
 		document.getElementById("email").value = "";
 		document.getElementById("email").placeholder = "Email is invalid";
 		document.getElementById("email").focus();
--- js/filter.js
+++ js/filter.js
@@ -1,5 +1,5 @@
 function filterView() {
-
+	// Hides rows depending on user specified species and gender
 	let tableRows = document.getElementsByTagName("tr");
 	for (let row = 0; row < tableRows.length; row++) {
 		tableRows[row].style.display = 'block';
@@ -48,52 +48,7 @@ function filterView() {
 	}
 }
 
-function test() {
-	//	for (let i = 1; i <= 6; i++) {
-	if (document.getElementById("animalType").value == 'all') {
-		document.getElementById("tr" + i).style.display = 'block';
-		if (document.getElementById("femaleRadio").checked) {
-			if (document.getElementById("tr" + i).classList.contains("male")) {
-				document.getElementById("tr" + i).style.display = 'none';
-			}
-		}
-		if (document.getElementById("maleRadio").checked) {
-			if (document.getElementById("tr" + i).classList.contains("female")) {
-				document.getElementById("tr" + i).style.display = 'none';
-			}
-		}
-	}
-	if (document.getElementById("animalType").value == 'cat') {
-		document.getElementById("tr" + i).style.display = 'block';
-		if (document.getElementById("tr" + i).classList.contains("dog")) {
-			document.getElementById("tr" + i).style.display = 'none';
-		} else if (document.getElementById("femaleRadio").checked) {
-			if (document.getElementById("tr" + i).classList.contains("male")) {
-				document.getElementById("tr" + i).style.display = 'none';
-			}
 
-		} else if (document.getElementById("maleRadio").checked) {
-			if (document.getElementById("tr" + i).classList.contains("female")) {
-				document.getElementById("tr" + i).style.display = 'none';
-			}
-		}
-	}
-	if (document.getElementById("animalType").value == 'dog') {
-		document.getElementById("tr" + i).style.display = 'block';
-		if (document.getElementById("tr" + i).classList.contains("cat")) {
-			document.getElementById("tr" + i).style.display = 'none';
-		} else if (document.getElementById("femaleRadio").checked) {
-			if (document.getElementById("tr" + i).classList.contains("male")) {
-				document.getElementById("tr" + i).style.display = 'none';
-			}
-
-		} else if (document.getElementById("maleRadio").checked) {
-			if (document.getElementById("tr" + i).classList.contains("female")) {
-				document.getElementById("tr" + i).style.display = 'none';
-			}
-		}
-	}
-}
 
 
 
